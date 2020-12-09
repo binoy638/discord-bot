50da3af6352530fd32697c4b1f17bcfb553cb823
binoy638 <binoy638@gmail.com>
2020-12-09 11:56:54 +0530

    t

--- src/commands/server.js
+++ src/commands/server.js
@@ -1,9 +1,10 @@
 module.exports = {
-	name: 'server',
-	description: 'Show server description',
-	cooldown: '6',
-	execute(message, args) {
-        message.channel.send(`This server's name is: ${message.guild.name}`);
-             
-	},
+  name: "server",
+  description: "Show server description",
+  cooldown: "6",
+  execute(message, args) {
+    message.channel.send(
+      `This server's name is: ${message.guild.name} OMEGALUL`
+    );
+  },
 };
