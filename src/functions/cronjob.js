639d2817e22bcea8d1e1dddbf54427b1ee1eca01
binoy638 <binoy638@gmail.com>
2021-01-12 15:24:28 +0530

    bug

--- src/functions/cronjob.js
+++ src/functions/cronjob.js
@@ -6,22 +6,16 @@ const getcronjob = require("./getcronjob");
 module.exports = async (section, interval, channel) => {
   const nineGagObject = new NineGag(section);
   const JobExist = getcronjob(channel.id);
-  var job = new CronJob(
-    `0 */${interval} * * * *`,
-    async function () {
-      let resp = await nineGagObject.getRandomPost(5);
-      sendgag(resp, channel);
-    },
-    null,
-    true
-  );
+  var job = new CronJob(`0 */${interval} * * * *`, async function () {
+    let resp = await nineGagObject.getRandomPost(5);
+    sendgag(resp, channel);
+  });
 
   // job.start();
   if (JobExist === true) {
     console.log("Existing job found");
-    cache.addCronJob("Cron Jobs", channel.id, job);
+    cache.replaceCronJob("Cron Jobs", channel.id, job);
     cache.findCronJob("Cron Jobs", channel.id).start();
-
     // current_job.start();
   } else {
     // console.log("No Existing job found");
