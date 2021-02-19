8c2027e3eecb7cb9a6151124b1b7074c774e3fb5
binoy638 <binoy638@gmail.com>
2021-02-19 17:34:06 +0530

    bot leave if empty channel

--- src/bot.js
+++ src/bot.js
@@ -104,5 +104,23 @@ client.on("guildMemberAdd", (member) => {
   onJoin(member);
 });
 
+client.on("voiceStateUpdate", (oldState, newState) => {
+  // if nobody left the channel in question, return.
+  if (
+    oldState.channelID !== oldState.guild.me.voice.channelID ||
+    newState.channel
+  )
+    return;
+
+  // otherwise, check how many people are in the channel now
+  if (!oldState.channel.members.size - 1)
+    setTimeout(() => {
+      // if 1 (you), wait five minutes
+      if (!oldState.channel.members.size - 1)
+        // if there's still 1 member,
+        oldState.channel.leave(); // leave
+    }, 300000); // (5 min in ms)
+});
+
 // Login to discord bot token
 client.login(process.env.TOKEN);
