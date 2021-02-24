6541da166c3db227bfa7fda8a41255ce93ff2e9c
binoy638 <binoy638@gmail.com>
2021-02-24 22:58:02 +0530

    bot dc bug

--- src/functions/voice/message.js
+++ src/functions/voice/message.js
@@ -3,9 +3,9 @@ module.exports = (channel) => {
   const embed = new Discord.MessageEmbed()
     .setTitle("Bumblebee Voice Assistant")
     .setAuthor("⚠️ This is an experimental feature.")
-    .setDescription("Currently Available voice commands.")
-    .addField('Bumblebee play music "music title"', "`Play a song.`")
-    .addField("Bumblebee pause music", "`Pause currently playing song.`")
-    .addField("Bumblebee resume music", "`Resume currently paused song.`");
+    .setDescription("Connected.");
+  // .addField('Bumblebee play music "music title"', "`Play a song.`")
+  // .addField("Bumblebee pause music", "`Pause currently playing song.`")
+  // .addField("Bumblebee resume music", "`Resume currently paused song.`");
   channel.send(embed);
 };
