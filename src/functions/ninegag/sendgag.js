cf6ca4e0696da7979e8ecc107a30721797fd9ab0
binoy638 <binoy638@gmail.com>
2021-03-10 17:07:53 +0530

    post title len bug

--- src/functions/ninegag/sendgag.js
+++ src/functions/ninegag/sendgag.js
@@ -2,6 +2,9 @@ const Discord = require("discord.js");
 const { htmlUnescape } = require("escape-goat");
 module.exports = (data, channel) => {
   let Title = htmlUnescape(data.title);
+  if (Title.length > 250) {
+    Title = Title.slice(0, 50) + "....";
+  }
   const gag = new Discord.MessageEmbed()
     .setColor("#0099ff")
     .setTitle(Title)
