066ff95aabbf05884e821fce3479ddee4c869368
binoy638 <binoy638@gmail.com>
2021-03-29 17:55:03 +0530

    added pm2 and restart command

---
+++ src/commands/misc/restart.js
@@ -0,0 +1,19 @@
+const Commando = require("discord.js-commando");
+
+module.exports = class AddCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "restart",
+      group: "misc",
+      memberName: "restart",
+      description: "Restart the bot.",
+      //   ownerOnly: true,
+    });
+  }
+  async run(message) {
+    setTimeout(() => {
+      process.exit();
+    }, 2000);
+    message.reply("Restarting the bot...");
+  }
+};
